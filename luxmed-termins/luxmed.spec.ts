41a7e1e92584d0f7e3c89d2afc0c7b7e2d4070ff
urgn <yuriy.raychinets@gmail.com>
2022-11-20 21:21:52 +0100

    first commit

---
+++ luxmed-termins/luxmed.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("https://www.luxmed.pl/");
+  await page.getByRole("button", { name: "Zaloguj" }).click();
+  await page
+    .locator("#header")
+    .getByRole("button", { name: "Portal Pacjenta" })
+    .click();
+  await page.getByPlaceholder("Login").fill(process.env.LUXMED_LOGIN);
+  await page.getByPlaceholder("Hasło").fill(process.env.LUXMED_PASSWORD);
+  await page.getByRole("button", { name: "Zaloguj się" }).click();
+  await page.getByRole("button", { name: "Umów usługę" }).click();
+  await page.locator(".click-area").click();
+  await page.getByText("Stomatologia").click();
+  await page.locator("#parent_4").getByText("Stomatolog").nth(2).click();
+  await page.getByRole("button", { name: "Szukaj" }).click();
+  for (let day = 1; day <= 14; day++) {
+    expect(
+      await page
+        .locator(`div:nth-child(${day}) > .card > .card-body > span`)
+        .textContent()
+    ).toMatch(/x|0/);
+  }
+});
