d29dc629c9acef1377d72c68b707227be7e9861b
urgn <yuriy.raychinets@gmail.com>
2022-12-06 00:57:17 +0100

    skip unused

--- luxmed-termins/pediatra.spec.ts
+++ luxmed-termins/pediatra.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("Pediatra", async ({ page }) => {
+test.skip("Pediatra", async ({ page }) => {
   await page.goto("https://www.luxmed.pl/");
   await page.getByRole("button", { name: "Zaloguj" }).click();
   await page
