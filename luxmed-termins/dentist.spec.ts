4c32814eec68f33601340eb1979eac9258795b3a
urgn <yuriy.raychinets@gmail.com>
2022-12-24 22:23:11 +0100

    higienistka fix

--- luxmed-termins/dentist.spec.ts
+++ luxmed-termins/dentist.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test.skip("dentist", async ({ page }) => {
+test("dentist", async ({ page }) => {
   await page.goto("https://www.luxmed.pl/");
   await page.getByRole("button", { name: "Zaloguj" }).click();
   await page
