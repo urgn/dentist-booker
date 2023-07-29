2ff6a2d5fcd031ba2bd9f704fb411fa00df9bece
urgn <yuriy.raychinets@gmail.com>
2023-07-29 21:01:55 +0200

    fix(termins): disable dentist

--- luxmed-termins/dentist.spec.ts
+++ luxmed-termins/dentist.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("Dentist", async ({ page }) => {
+test.skip("Dentist", async ({ page }) => {
   await page.goto("https://www.luxmed.pl/");
   await page.getByRole("button", { name: "Zaloguj" }).click();
   await page
